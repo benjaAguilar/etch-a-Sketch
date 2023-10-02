438579d743e015cbb998ee4d81f3a7f5ddbc9933
benjaAguilar <benjaaguilar02@gmail.com>
2023-10-02 07:25:03 -0300

    Added a function printGrid and size variables
    now you can choose the size of the canvas calling printGrid
    who recives a parameter of the size

--- js/e-a-t_script.js
+++ js/e-a-t_script.js
@@ -1,18 +1,31 @@
 let canvas = document.querySelector('.canvas');
+let size_x16 = 16 * 16;
+let size_x32 = 32 * 32;
+let size_x64 = 64 * 64;
 
-for(let i = 1; i <= 255; i++){
-    let div = document.createElement('div');
-    div.classList.add("grid");
+printGrid(size_x16);
 
-    canvas.appendChild(div);
-}
+function printGrid(gridSize){
+    document.querySelectorAll('.grid').forEach((grid) => {
+        grid.remove();
 
-let grids = document.querySelectorAll('.grid');
-
-grids.forEach((grid) => {
-    grid.addEventListener("mouseover", () => {
-        grid.style.backgroundColor = "#333";
-        
     });
 
-});
+    for(let i = 1; i < gridSize; i++){
+        let div = document.createElement('div');
+        div.classList.add("grid");
+    
+        canvas.appendChild(div);
+    }
+    
+    let grids = document.querySelectorAll('.grid');
+
+    grids.forEach((grid) => {
+        grid.addEventListener("mouseover", () => {
+            grid.style.backgroundColor = "#333";
+    
+        });
+    
+    });
+}
+
